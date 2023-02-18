e1e53073e845d3df04e79dfe98e002a66a8f6099
GardenerX <m0gaupr@gmail.com>
2023-02-18 08:56:56 -0800

    almost done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Program that prints a string/line without using printf or puts
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+fwrite(1, "and that piece of art", strlen("and that piece of art"));
+fwrite(1, "is useful\"  - Dora", strlen("is useful\"  - Dora"));
+fwrite(1, "Korpar, 2015 -10 - 19", strlen("Korpar, 2015 - 10 - 19"));
+return (1);
+}
