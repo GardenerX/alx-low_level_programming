035fe60ffd2ee139c069f8f884a7a818a3cf91f1
GardenerX <m0gaupr@gmail.com>
2023-02-18 03:50:04 -0800

    wellwell

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -  C program that prints a string/line using put(s)
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints a string/line using printf()
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
