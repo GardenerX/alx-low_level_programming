dcd0da6a5f68f9249aae025eed409e9db6772bb8
GardenerX <m0gaupr@gmail.com>
2023-02-24 06:00:06 -0800

    mow

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * description: from z - a
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char m;
+m = 'z';
+
+while
+(m <= 'z') {
+if
+(m >= 'a')
+putchar(m);
+m--;
+}
+putchar('\n');
+
+return (0);
+}
