9fef47b31242113269e59a5a67ad25bdebe70315
GardenerX <m0gaupr@gmail.com>
2023-02-24 06:49:04 -0800

    er

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+/**
+ * main - Entry Point
+ *
+ * main : print 0-9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int e;
+e = 0;
+while
+(e < 9)
+{
+putchar(e + '0');
+e++;
+putchar(',');
+}
+if
+(9 < 10)
+{
+putchar('9');
+}
+putchar('\n');
+
+return (0);
+}
