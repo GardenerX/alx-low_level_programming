b43e54d3ccc11daed043a52c6de9c9b524a2ec2a
GardenerX <m0gaupr@gmail.com>
2023-02-24 01:28:29 -0800

    me

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry Point
+ *
+ * main : print 0-9
+ *
+ * Return : Always 0 (success)
+ */
+
+int main(void)
+{
+int n = 0;
+
+for (n = 0; n < 10; n++)
+printf("%d", n);
+
+putchar('\n');
+return (0);
+}
