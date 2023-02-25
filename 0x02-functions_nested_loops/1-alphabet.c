d01088ccb03b73b072054ab8647bf61077860315
GardenerX <m0gaupr@gmail.com>
2023-02-24 23:14:32 -0800

    sn

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ *
+ * defintion: print alphabets in lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int n;
+n = 'a';
+while
+(n <= 'z')
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+
+return;
+}
