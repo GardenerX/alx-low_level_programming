e3d9882a27cc0365dbe997cdca68af0f98418adf
GardenerX <m0gaupr@gmail.com>
2023-02-28 05:43:15 -0800

    we

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,18 +6,11 @@
  * description: reset value to 98
  * 'n' -is an int variable
  * @n:input
- * @n:output
- * Return: 0 (success)
+ *
+ * Return: n (success)
  */
 
 void reset_to_98(int *n)
 {
-int i;
-n = &i;
-*n = 402;
-
-printf("%d\n", i);
-reset_to_98(&i);
-printf("%d\n", *n);
-
+*n = 98;
 }
