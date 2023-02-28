ff47572b6242bb3dd40b3e2c0772f4ecfeab2fa2
GardenerX <m0gaupr@gmail.com>
2023-02-28 07:16:24 -0800

    we

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s:input
+ *
+ * Return: 0 (success)
+ */
+int _strlen(char *s)
+{
+int str;
+char length;
+s = &length;
+*s = sizeof(str);
+return (0);
+}
