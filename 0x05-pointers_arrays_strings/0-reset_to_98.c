99c002176c7d2980ceb5b0b37e0698b4ea5fdd7c
GardenerX <m0gaupr@gmail.com>
2023-02-28 00:22:53 -0800

    seee

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry Point
+ *
+ * description: reset value to 98
+ * 'n' -is an int variable
+ * @n:input
+ * Return: n
+ */
+
+void reset_to_98(int *n);
+{
+*n = 98
+}
