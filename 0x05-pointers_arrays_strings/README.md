6e938b29eedc996ae316a5c24f4c9a0e0c349c5e
GardenerX <m0gaupr@gmail.com>
2023-02-28 00:59:22 -0800

    we

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#READMEmd file for the the project 0x05-pointers_arrays_strings
+#It contains the following tasks
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int _putchar(char c);
+extern void reset_to_98(int *n);
+extern void swap_int(int *a, int *b);
+extern int _strlen(char *s);
+extern void _puts(char *str);
+extern void print_rev(char *s);
+extern void rev_string(char *s);
+extern void puts2(char *str);
+extern void puts_half(char *str);
+extern void print_array(int *a, int n);
+extern char *_strcpy(char *dest, char *src);
+extern int _atoi(char *s);
+
+#endif /**MAIN_H*/
