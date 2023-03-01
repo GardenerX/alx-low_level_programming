07bcf6a4c236811b62b5bda5fa41700dbff738c4
GardenerX <m0gaupr@gmail.com>
2023-03-01 12:49:24 -0800

    qw

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of integer
+ * description: absolute
+ * @n:input
+ * Return: n
+ */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+return (-n);
+}
