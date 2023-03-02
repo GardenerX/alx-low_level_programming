5351424579e56d0e82804f01633edc39ae096c7d
GardenerX <m0gaupr@gmail.com>
2023-03-02 13:19:30 -0800

    we

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - where a letter is uppercase
+ * @c:input
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+if (c <= 90 && c >= 65)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
