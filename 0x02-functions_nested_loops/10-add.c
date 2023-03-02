2d7f886bcd0c389271ba7ccad32c58570149f9b1
GardenerX <m0gaupr@gmail.com>
2023-03-02 11:04:28 -0800

    we

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @int:input
+ * @+:addition
+ *
+ * Return: sum = a + b
+ */
+extern int add(int, int);
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
