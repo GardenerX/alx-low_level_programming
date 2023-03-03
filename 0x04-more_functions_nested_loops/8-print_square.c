5f86c855a76e23bb1c3e80af7cc527d9e536b955
GardenerX <m0gaupr@gmail.com>
2023-03-03 09:45:34 -0800

    we

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size:input
+ *
+ */
+void print_square(int size)
+{
+int h, p;
+if
+(size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (h = 1; h <= size; h++)
+{
+for (p = 1; p <= size; p++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
