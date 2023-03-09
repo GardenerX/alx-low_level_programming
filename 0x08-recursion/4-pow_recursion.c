56585f213ca097554dc31df4ad574ea99b756609
GardenerX <m0gaupr@gmail.com>
2023-03-09 13:25:39 -0800

    we

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - to the power of
+ * @x:input
+ * @y:input
+ *
+ * Return: 0 or -1
+ */
+int _pow_recursion(int x, int y)
+{
+int result = x;
+
+if (y < 0)
+return (-1);
+
+else if (y == 0)
+return (1);
+
+result *= _pow_recursion(x, y - 1);
+
+return (result);
+}
