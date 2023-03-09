00b63139b2d137453d7482ab7c18a21a31ad9cc0
GardenerX <m0gaupr@gmail.com>
2023-03-08 17:06:56 -0800

    we

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print string length
+ * @s:input
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+int length = 0;
+
+if (*s)
+{
+length++;
+length += _strlen_recursion(s + 1);
+}
+return (length);
+}
