bc7954a03dd6f9762b04b98dff1a92f7b5990f9e
GardenerX <m0gaupr@gmail.com>
2023-08-30 19:55:25 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
 if (*s)
 {
-_putchar(s[i]);
+_putchar(*s);
 _puts_recursion(s + 1);
 }
 _putchar('\n');
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-int i = 0;
 if (*s)
 {
 _print_rev_recursion(s + 1);
-_putchar(s[i]);
+_putchar(*s);
 }
 }
