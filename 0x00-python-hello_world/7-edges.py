f1dff1f9f5e15578d55ec30bf251c9dbfc925b4b
GardenerX <m0gaupr@gmail.com>
2023-11-27 11:07:23 -0800

    python

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[7:]
+words = word[1:-1]
+middle_word = words
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+b = open('./Zen', 'r')
+contents = b. read()
+print(contents)
+b. close()
